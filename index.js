f30b6256d27ec6b16ca607a1491e179d174bf857
mrarthurwhite <mister.arthur.white@gmail.com>
2020-04-06 14:02:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const testVar = {}
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+];
 
-function testFunc() {
-  return "hi"
+function superbowlWin(record) {
+
+    let winningRecord =  record.find(
+                          row=> {
+                            return (row.result=="W");
+                          }
+                      );
+    let year;
+    if (winningRecord){
+        year =  winningRecord.year;
+    }
+    return year;
 }
